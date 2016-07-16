febd26619925c1605696daf9ee33b71c3d3e59d7
Aslonski <slonski.andrey@gmail.com>
2016-07-16 16:41:19 -0500

    Add edge case handling method

--- Fixed Array/fixed_array.rb
+++ Fixed Array/fixed_array.rb
@@ -4,15 +4,30 @@ class FixedArray
 
 		def initialize(size)
 			@new_array = Array.new(size)
+			@length = @new_array.length
 		end
 
 		def get(index)
-			@new_array[index]
+			out_of_bounds?(index)
+			p @new_array[index]
 		end	
 
 		def set(index, element)
+			out_of_bounds?(index)
 			@new_array[index] = element
 		end	
 
+		private
+
+		def out_of_bounds?(index)
+			if index == nil || index > @length
+				throw "Out of bounds"
+				return
+			end
+		end
+
 end
 
+arr = FixedArray.new(5)
+arr.set(9, "Poop")
+arr.get(2)
